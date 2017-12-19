fb13c5a0e0453f139089c01a828f776be4e2430f
wansong <betterofsong@gmail.com>
2017-12-19 18:01:00 +0800

    1.0.2: android resolves to null

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 const ZKItunesApi = NativeModules.ZKItunesApi;
 
 export default function getAppMeta(appId) {
   return new Promise((resolve, reject) => {
+    if (Platform.OS !== 'ios') {
+      resolve(null);
+      return;
+    }
+
     ZKItunesApi.getAppMeta(appId, (error, meta) => {
       if (error) {
         reject(error);
